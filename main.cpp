28196d8af4b0a7684ca40a2f3f0b0f52c4da01ff
Ilhomjonewuu <meliboyev189@gmail.com>
2025-09-10 00:06:14 +0500

    problems 1-10

---
+++ main.cpp
@@ -0,0 +1,147 @@
+//problem1
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     int intnumber = 24;
+//     cout << "Value of integer is: " << intnumber
+//          << ". Size is: " << sizeof(intnumber) << endl;
+//
+//     float floatnumber = 3.14;
+//     cout << "Value of float is: " << floatnumber
+//          << ". Size is: " << sizeof(floatnumber) <<  endl;
+//
+//     double doublenumber = 45.1234;
+//     cout << "Value of double is: " << doublenumber
+//          << ". Size is: " << sizeof(doublenumber) <<  endl;
+//
+//     bool boolean = true;
+//     cout << "Value of boolean is: " << boolean
+//          << ". Size is: " << sizeof(boolean) <<  endl;
+//
+//     char charname = 'A';
+//     cout << "Value of char is: " << charname
+//          << ". Size is: " << sizeof(charname)  << endl;
+//
+//     return 0;
+// }
+//problem2
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main() {
+//     string name="Alijon Aliev";
+//     cout<<name<<endl;
+//     int id=233242;
+//     cout<<id<<endl;
+//     string favsong="Queen - The Show Must Go On";
+//     cout<<favsong<<endl;
+//     string movie="Titanic";
+//     cout<<movie<<endl;
+//     return 0;
+//
+// }
+//problem3
+// #include <iostream>
+// using namespace std;
+//
+// int main() {
+//     cout<<"\"I'm standing\" on the edge of some crazy cliff."
+//           "What I have to do, I have to catch everybody\\"
+//           "if they start to go over the cliff - I mean"
+//           "\\if they're running and they don't look where they're going I have to come out from somewhere and catch them.";
+//     return 0;
+//
+// }
+//problem4
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a=189;
+//     char b='B';
+//     cout<<float(a/3)<<endl;
+//     cout<<int(b)<<endl;
+//     cout<<float((a+b)/5)<<endl;
+//     cout<<char(b+(a/10-1));
+//     return 0;
+// }
+//problem5
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a=12+38;
+//     int b=12*38;
+//     cout<<"Sum="<<a<<endl;
+//     cout<<"Product="<<b;
+//     return 0;
+// }
+//problem6
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int sum1;
+//     cout<<"value1: "<<endl;
+//     cin>>sum1;
+//     cout<<"value2: "<<endl;
+//     int sum2;
+//     cin>>sum2;
+//     cout<<"Sum: "<<sum1<<"+"<<sum2<<"="<<sum1+sum2;
+//     return 0;
+// }
+//problem7
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x;
+//     cin>>x;
+//     int y;
+//     cin>>y;
+//     cout<<"t="<<3*(x*x+3)/(y/4+3);
+//     return 0;
+// }
+//problem8
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main() {
+//     float pi=3.14;
+//     float d;
+//     cin>>d;
+//     cout<<"Area: "<<pi*d*d;
+//     return 0;
+// }
+//problem9
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     float v0;
+//     cin>>v0;
+//     float v1;
+//     cin>>v1;
+//     int t;
+//     cin>>t;
+//     double a=(v1-v0)/t;
+//     cout<<a;
+//     return 0;
+// }
+//problem10
+// #include <iostream>
+// #include <string>
+// using namespace std;
+//
+//
+// int main() {
+//     float d;
+//     cout<<"The distance to drive: "<<endl;
+//     cin>>d;
+//     float fe;
+//     cout<<"The fuel efficiency of the car in miles per gallon: "<<endl;
+//     cin>>fe;
+//     float price;
+//     cout<<"the price per gallon: "<<endl;
+//     cin>>price;
+//     cout<<"The cost of driving is "<<d*price/fe;
+//     return 0;
+//
+//
+// }
